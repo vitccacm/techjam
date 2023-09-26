0cb2885b0289c5e556cc81decb9fdfd6ff58aec1
HaranK007 <shreeharan2003@gmail.com>
2023-09-26 13:46:29 +0530

    mock api

---
+++ src/loginMockApi.js
@@ -0,0 +1,20 @@
+const users = [
+    { id: 1, username: 'admin', password: 'admin' },
+    { id: 2, username: 'user', password: 'password' },
+  ];
+  
+  let currentUser = null;
+  
+  export function login(username, password) {
+    const user = users.find((u) => u.username === username && u.password === password);
+    if (user) {
+      currentUser = user;
+      return user;
+    }
+    return null;
+  }
+  
+  export function getCurrentUser() {
+    return currentUser;
+  }
+  
