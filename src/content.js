4ff28d1379a76939e43818e6cda2bec8af366b77
betty5634 <g820255@gmail.com>
2023-09-29 14:34:04 +0530

    update

--- src/content.js
+++ src/content.js
@@ -122,7 +122,7 @@ const Content=()=>{
                 
                   
                   <Table3/>
-                  <div style={{display:'flex',flexDirection:'row',gap:'20px',justifyContent:'center',alignItems:'center',margin:'20px'}}>
+                  <div style={{display:'flex',flexDirection:'row',gap:'20px',justifyContent:'center',alignItems:'center',margin:'20px',backgroundColor:'#252525'}}>
                   
                   <a href="https://vitchennai.acm.org/divide-and-conquer.html">
                   <button>
