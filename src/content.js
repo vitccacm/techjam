f6fb4b898d10ab75d92e4aa2091a4837316e6f1c
betty5634 <g820255@gmail.com>
2023-09-29 13:51:44 +0530

    day3up

--- src/content.js
+++ src/content.js
@@ -6,6 +6,7 @@ import { AlignCenterHorizontalSimple,CaretDown, CaretLeft } from "@phosphor-icon
 import TableNew from "./tablenew";
 import Table2 from "./Table2";
 import Table3 from "./Table3";
+import './button.css'
 
 const Content=()=>{
     const [visible,setvisible]= React.useState(true)
@@ -118,10 +119,30 @@ const Content=()=>{
           </div>
           <div>
                 {visible3 && (<Paper className='table' sx={{backgroundColor: 'black'}}>
-   
-          <Table3/>
+                <div>
+                <h1 id="divide-and-conquer">Divide and Conquer</h1>
+                
+                </div>
+                  
+                  <Table3/>
+                  <div style={{display:'flex',flexDirection:'row',gap:'20px'}}>
+                  
+                  <a href="https://vitchennai.acm.org/divide-and-conquer.html">
+                  <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path></svg>
+                <span>Learn More About Divide & Conquer</span>
+                 </button>
+                 </a>
+                  
+                 <a href="https://vitchennai.acm.org/graphs-and-trees.html">
+                 <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path></svg>
+                <span>Learn More About Graph & Trees</span>
+                 </button>
+                 </a>
+                  </div>
       
-    </Paper>)}
+                  </Paper>)}
     
     
                 </div>
